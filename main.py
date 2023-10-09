b1e0686e5162400a7673a669524e78c21f402b34
Brady Trudel <bradytrudel@outlook.com>
2023-10-09 17:25:57 -0400

    added main file

---
+++ main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+root = Tk()
+root.title('Eaglercraft 1.20')
+root.geometry('800x700')
+
+Label(text='Eaglercraft 1.20 coming soon!').pack()
+
+
+
+
+
+
+
+root.mainloop()
