4a57e3922b69760c71da66e832f92d17f114a9f2
Brady Trudel <bradytrudel@outlook.com>
2023-10-09 17:59:32 -0400

    adding loading screen...

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from tkinter import *
+from time import sleep
 
 
 root = Tk()
 root.title('Eaglercraft 1.20')
-root.geometry('800x700')
+root.geometry('800x600')
 
-Label(text='Eaglercraft 1.20 coming soon!').pack()
+def do_eagtek_screen():
+    eagtek_photo = PhotoImage(file='assets/start-screen/eagtek.png')
+    eagtek_label = Label(image=eagtek_photo)
+    eagtek_label.pack()
+
+    root.update()
+    root.after(2000)
+    eagtek_label.destroy()
+
+
+do_eagtek_screen()
+
+enable_photo = PhotoImage(file='assets/start-screen/enable.png')
+enable_label = Label(image=enable_photo)
+enable_label.pack()
 
 
 
