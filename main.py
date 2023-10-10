cfa1ed262572b3f18b5f3df836f52456687e0aad
Brady Trudel <bradytrudel@outlook.com>
2023-10-10 07:29:34 -0400

    importing assets... DONT TOUCH

--- main.py
+++ main.py
@@ -7,15 +7,14 @@ root.title('Eaglercraft 1.20')
 root.geometry('800x600')
 
 def extract_png_asset(asset_name):
-    return PhotoImage('assets/' + asset_name + '.png')
+    return PhotoImage(file='assets/' + asset_name + '.png')
 
-panorama_photo = extract_png_asset('panorama')
 titlescreen_top_photo = extract_png_asset('titlescreen_top')
 dirt_bg_photo = extract_png_asset('dirt_bg')
 download_terrian_photo = extract_png_asset('download_terrain')
 original_server_photo = extract_png_asset('original_server')
-panorama_photo = extract_png_asset('panorama')
-timed_out_photo = extract_png_asset('times_out')
+titlescreen_bg_photo = extract_png_asset('titlescreen_bg')
+timed_out_photo = extract_png_asset('timed_out')
 back_2_titlescreen_photo = extract_png_asset('buttons/back_2_titlescreen')
 cancel_photo = extract_png_asset('buttons/cancel')
 done_photo = extract_png_asset('buttons/done')
@@ -26,8 +25,8 @@ realms_photo = extract_png_asset('buttons/realms')
 singleplayer_photo = extract_png_asset('buttons/singleplayer')
 
 
-
-
+dirt_bg = Label(image=dirt_bg_photo)
+dirt_bg.pack()
 
 
 
